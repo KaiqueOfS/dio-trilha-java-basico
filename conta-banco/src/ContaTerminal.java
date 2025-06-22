812059598bcd39ffd7e35416d3e32373fa098742
KaiqueOFS <kaiquo12@gmail.com>
2025-06-22 16:03:46 -0300

    Implementa funcionalidade de criação de conta no sistema bancário

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+       System.out.print("Por favor, digite o número da conta: ");
+        var numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        var agencia = scanner.nextLine();
+
+        System.out.print("Por favor, informe seu nome completo: ");
+        var nomeCliente = scanner.nextLine();
+
+        double saldo = 237.48;
+
+        //final
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero +" e seu saldo R$" + saldo+ " já está disponível para saque.");
+
+
+        scanner.close();
     }
 }
